3836427e37756cae71c251308fb378ad8033a91d
thermo <romain.chauveau+github@protonmail.com>
2023-12-04 01:16:06 +0100

    solve day 3 part 2 (need some refacto...)

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -6,29 +6,40 @@ use regex::Regex;
 advent_of_code::solution!(3);
 
 #[derive(Debug)]
-struct Part {
+struct Numberl {
     line: usize,
     range: Range<usize>,
     surrounding: String,
     value: i32,
 }
 
-impl Part {
+#[derive(Debug)]
+struct Part {
+    values: Vec<u32>,
+    symbol: String,
+    surrounding: Range<usize>,
+}
+
+#[derive(Debug)]
+struct Number {
+    value: u32,
+    positions: Range<usize>,
+}
+
+impl Number {
+    fn intersect(&self, range: Range<usize>) -> bool {
+        self.positions.start <= range.end && range.start <= self.positions.end
+    }
+}
+
+impl Numberl {
     fn is_mechanical(&self) -> bool {
         static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"[^\d\\.]").unwrap());
         RE.is_match(&*self.surrounding)
     }
 }
 
-
-pub fn part_one(input: &str) -> Option<u32> {
-    let grid = input.lines().collect::<Vec<_>>();
-    let reg = Regex::new(r"(\d+)").unwrap();
-    let results = get_parts(&grid, reg);
-    Some(results.iter().filter(|p| p.is_mechanical()).map(|p| p.value as u32).sum())
-}
-
-fn get_parts(grid: &Vec<&str>, reg: Regex) -> Vec<Part> {
+fn get_parts(grid: &Vec<&str>, reg: Regex) -> Vec<Numberl> {
     let mut results: Vec<_> = vec![];
     for (i, line) in grid.iter().enumerate() {
         let mut ranges: Vec<_> = reg
@@ -46,7 +57,7 @@ fn get_parts(grid: &Vec<&str>, reg: Regex) -> Vec<Part> {
                     .map(|s| String::from(&s[range_start..range_end]))
                     .collect::<Vec<_>>()
                     .join("");
-                Part { line: i, range: range.clone(), surrounding: String::from(surrounding), value: line[range].parse::<i32>().unwrap() }
+                Numberl { line: i, range: range.clone(), surrounding: String::from(surrounding), value: line[range].parse::<i32>().unwrap() }
             })
             .collect();
         results.append(&mut ranges);
@@ -54,8 +65,43 @@ fn get_parts(grid: &Vec<&str>, reg: Regex) -> Vec<Part> {
     results
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    let grid = input.lines().collect::<Vec<_>>();
+    let reg = Regex::new(r"(\d+)").unwrap();
+    let results = get_parts(&grid, reg);
+    Some(results.iter().filter(|p| p.is_mechanical()).map(|p| p.value as u32).sum())
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = input.lines().collect::<Vec<_>>();
+    let part_reg = Regex::new(r"[^\d\\.]").unwrap();
+    let num_reg = Regex::new(r"\d+").unwrap();
+    let mut results: Vec<Part> = vec![];
+    for (i, line) in grid.iter().enumerate() {
+        let mut result = part_reg
+            .find_iter(line)
+            .map(|m| {
+                let surr_start = if i > 1 { i - 1 } else { 0 };
+                let surr_end = if i < grid.len() - 1 { i + 2 } else { grid.len() };
+                let mut part = Part { values: vec![], symbol: String::from(&line[m.range()]), surrounding: m.range() };
+                for l in &grid[surr_start..surr_end] {
+                    let mut values = num_reg.find_iter(l)
+                        .map(|m2| Number { value: (&l[m2.range()]).parse().unwrap(), positions: m2.range() })
+                        .filter(|n| n.intersect(part.surrounding.clone()))
+                        .map(|n| n.value)
+                        .collect::<Vec<_>>();
+                    part.values.append(&mut values);
+                }
+                part
+            })
+            .collect::<Vec<_>>();
+        results.append(&mut result);
+    }
+    Some(results
+        .iter()
+        .filter(|p| p.values.len() > 1 && p.symbol == "*")
+        .map(|p| p.values[0] * p.values[1])
+        .sum())
 }
 
 #[cfg(test)]
@@ -71,6 +117,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
