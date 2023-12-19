3c375e801993812d95b5ee08b0e79cf2e1af157f
romain.chauveau <romain.chauveau@praxedo.com>
2023-12-19 18:19:01 +0100

    solve day 13 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,74 +1,48 @@
-use std::cmp;
 use colored::Colorize;
-use itertools::{Itertools, min};
+use itertools::Itertools;
+
 advent_of_code::solution!(13);
 
-fn get_possible_vertical_reflection(input: &str, possiblities: Vec<usize>) -> Vec<usize> {
-    // println!("{} - {:?}", input, possiblities);
-    let mut possible_reflections: Vec<usize> = vec![];
-    for i in possiblities {
-        let length = cmp::min(i, input.len() - i);
-        let (left, right) = input.split_at(i);
-        let left_mirrored = left.chars().rev().collect::<String>();
-        if &left_mirrored[0..length] == &right[0..length] {
-            possible_reflections.push(i);
-        }
-        // println!("[{}]{}|{} - {}|{} -> {}", i, &input[0..i], &input[i..input.len()], &left_mirrored[0..length], &right[0..length], &left_mirrored[0..length] == &right[0..length]);
-    }
-    possible_reflections
+fn reflects_at(input: &Vec<Vec<char>>, expected_diff: usize) -> Option<usize> {
+    (1..input.len()).find(|&offset| {
+        let top_half = input.iter().take(offset).rev();
+        let bottom_half = input.iter().skip(offset);
+        let binding = top_half.zip(bottom_half);
+        let found_diff: usize = binding
+            .map(|(row1, row2)| row1.iter().zip(row2.iter()).filter(|(a, b)| a != b).count())
+            .sum();
+        found_diff == expected_diff
+    })
 }
 
-fn get_vertical_reflection(input: &str) -> Option<usize> {
-    let lines = input.lines().collect_vec();
-    let mut current_range = (1..lines[0].len()).collect_vec();
-    for line in lines {
-        let possible_reflections = get_possible_vertical_reflection(line, current_range);
-        if possible_reflections.len() == 0 {
-            return None;
-        }
-        current_range = possible_reflections;
-    }
-    Some(current_range[0])
-}
-
-fn get_horizontal_reflection(input: &str) -> Option<usize> {
-    let binding = transpose2(input.lines().map(|l| l.chars().collect_vec()).collect_vec());
-    let second_pattern = binding.iter().map(|chars| chars.iter().collect::<String>()).collect_vec().join("\n");
-    get_vertical_reflection(&second_pattern)
-}
-
-fn transpose2<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
-    assert!(!v.is_empty());
-    let len = v[0].len();
-    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
-    (0..len)
-        .map(|_| {
-            iters
-                .iter_mut()
-                .map(|n| n.next().unwrap())
-                .collect::<Vec<T>>()
+fn process(input: &str, expected_diff: usize) -> usize {
+    input.split("\n\n")
+        .map(|pattern| {
+            let pattern_vec = pattern.lines().map(|l| l.chars().collect_vec()).collect_vec();
+            if let Some(i) = reflects_at(&pattern_vec, expected_diff) {
+                return i * 100;
+            }
+            let cols = (0..pattern_vec[0].len())
+                .map(|i| pattern_vec.iter().map(|row| row[i]).collect_vec())
+                .collect_vec();
+            if let Some(i) = reflects_at(&cols, expected_diff) {
+                return i;
+            }
+            0
         })
-        .collect()
+        .sum::<usize>()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let result = input.split("\n\n")
-        .map(|line| {
-            let vertical_reflection = get_vertical_reflection(line);
-            return if vertical_reflection.is_some() {
-                vertical_reflection.unwrap()
-            } else {
-                let horizontal_reflection = get_horizontal_reflection(line);
-                horizontal_reflection.unwrap() * 100
-            }
-        })
-        .sum::<usize>();
+    let result = process(input, 0);
     println!("result = {:?}", result);
     Some(result as u64)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let result = process(input, 1);
+    println!("result = {:?}", result);
+    Some(result as u64)
 }
 
 #[cfg(test)]
@@ -84,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }
